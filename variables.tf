8c678805e59049007ee0c10ac936d21fe17b8492
Anudeep varma <datlanagaanudeep@gmail.com>
2025-07-18 22:56:00 +0530

    added new repo

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+  default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+  default = "Z053279234NT99R7HQSG4"
+}
+
+variable "zone_name" {
+  default = "deeps.sbs"
+}
+
+variable "component" {
+
+}
