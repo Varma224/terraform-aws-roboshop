68bbcf1bdb193911ecdf43ca63f185dddf39b50c
Anudeep varma <datlanagaanudeep@gmail.com>
2025-07-19 12:00:13 +0530

    added sg id value

--- locals.tf
+++ locals.tf
@@ -1,6 +1,7 @@
 locals {
     ami_id = data.aws_ami.joindevops.id
     vpc_id = data.aws_ssm_parameter.vpc_id.value
+    sg_id = data.aws_ssm_parameter.sg_id.value
 
     private_subnet_id = split("," , data.aws_ssm_parameter.private_subnet_ids.value)[0]
     private_subnet_ids = split("," , data.aws_ssm_parameter.private_subnet_ids.value)
